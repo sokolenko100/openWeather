b2a495e4ceba27054a3fdc02ac315b60aa29643b
Artem <sokolenko100@gmail.com>
2024-09-20 11:38:42 +0200

    refactoring code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
     root: true,
+
     extends: [
+        '@react-native-community',
+        'plugin:jest/recommended',
         'plugin:import/typescript',
         '@react-native',
         'prettier',
@@ -18,29 +21,29 @@ module.exports = {
         tsconfigRootDir: './',
     },
     ignorePatterns: ['/*.*', 'FontsBase64.js'],
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'jest'],
     rules: {
         'no-undef': 'warn',
-        'linebreak-style': 'off',
-        'eslint-comments/no-unlimited-disable': 'off',
-        '@typescript-eslint/ban-ts-ignore': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/no-unsafe-call': 'off',
-        '@typescript-eslint/no-unsafe-return': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
-        '@typescript-eslint/no-unsafe-argument': 'off',
-        '@typescript-eslint/restrict-plus-operands': 'off',
-        '@typescript-eslint/restrict-template-expressions': 'off',
-        'react-hooks/exhaustive-deps': 'off',
+        'linebreak-style': 'warn',
+        'eslint-comments/no-unlimited-disable': 'warn',
+        '@typescript-eslint/ban-ts-ignore': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+        '@typescript-eslint/no-unsafe-call': 'warn',
+        '@typescript-eslint/no-unsafe-return': 'warn',
+        '@typescript-eslint/no-empty-function': 'warn',
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+        '@typescript-eslint/restrict-plus-operands': 'warn',
+        '@typescript-eslint/restrict-template-expressions': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
         '@typescript-eslint/no-empty-interface': [
             'error',
             {
                 allowSingleExtends: true,
             },
         ],
-        'no-void': 'off',
+        'no-void': 'warn',
         'prettier/prettier': [
             'error',
             {
@@ -48,7 +51,7 @@ module.exports = {
             },
         ],
         'object-curly-spacing': ['error', 'always'],
-        'no-shadow': 'off',
+        'no-shadow': 'warn',
         '@typescript-eslint/no-shadow': ['error'],
     },
     globals: {
@@ -57,4 +60,12 @@ module.exports = {
     env: {
         'jest/globals': true,
     },
+    overrides: [
+        {
+            files: ['**/__tests__/**/*.ts', '**/__tests__/**/*.tsx'],
+            env: {
+                'jest/globals': true,
+            },
+        },
+    ],
 };
